77559dfcf17960597228ea265e613470d8e1d912
Thomas Watson Steen <w@tson.dk>
2017-02-03 13:01:43 +0100

    Normalize blocks so each block equal one version

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
 'use strict'
 
 var ChangesStream = require('changes-stream')
+var clean = require('normalize-registry-metadata')
+var through2 = require('through2')
 var hypercore = require('hypercore')
 var swarm = require('hyperdrive-archive-swarm')
 var ndjson = require('ndjson')
 var pump = require('pump')
 var levelup = require('level')
+
+var normalize = through2.obj(function (change, env, cb) {
+  var stream = this
+  var doc = clean(change.doc)
+
+  if (!doc) {
+    console.log('skipping %s - invalid document', change.id)
+  } else if (!doc.versions || doc.versions.length === 0) {
+    console.log('skipping %s - no versions detected', change.id)
+  } else {
+    Object.keys(doc.versions).forEach(function (version) {
+      stream.push(doc.versions[version])
+    })
+  }
+
+  cb()
+})
 
 var db = levelup(process.argv[2] || './npm-to-hypercore.db')
 var core = hypercore(db)
@@ -27,12 +46,12 @@ core.list(function (err, keys) {
     var stream = feed.createWriteStream()
 
     if (feed.blocks === 0) {
-      pump(changesSinceStream(), stream, run)
+      pump(changesSinceStream(), normalize, ndjson.serialize(), stream, run)
     } else {
       feed.get(feed.blocks - 1, function (err, block) {
         if (err) throw err
         var seq = JSON.parse(block).seq
-        pump(changesSinceStream(seq), stream, run)
+        pump(changesSinceStream(seq), normalize, ndjson.serialize(), stream, run)
       })
     }
   }
@@ -43,12 +62,10 @@ function changesSinceStream (seq) {
 
   console.log('feching changes since sequence #%s', seq)
 
-  var changes = new ChangesStream({
+  return new ChangesStream({
     db: 'https://replicate.npmjs.com',
     include_docs: true,
     since: seq,
     highWaterMark: 4 // reduce memory - default is 16
   })
-
-  return changes.pipe(ndjson.serialize())
 }
